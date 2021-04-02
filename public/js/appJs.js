3afb4f78338c5fe686709763d015bfe2bfcd4d70
Jarousseau_Herve <jarousseau.herve@gmail.com>
2021-04-02 13:52:52 +0200

    poursuite de la fonctionnalité "like" (HJ)

--- public/js/appJs.js
+++ public/js/appJs.js
@@ -1,3 +1,24 @@
 
 
+// event clic heart !
+var likedElement = document.querySelector(".likePerson");
+likedElement.addEventListener("click", sendLike);
+
+
+
+function sendLike( event ) {
+    let clickedElt = event.currentTarget;
+    let userIdLiked = clickedElt.dataset.userId;
+
+    axios.post('http://localhost/SiteDeRencontre/siteRencontre/public/reaction', {
+        "id": userIdLiked
+    })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+}
 
--- src/Controller/ReactionController.php
+++ src/Controller/ReactionController.php
@@ -2,14 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Profile;
 use App\Entity\Reaction;
+use App\Entity\User;
+use App\Repository\ProfileRepository;
 use App\Repository\ReactionRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class ReactionController extends AbstractController
@@ -17,21 +23,32 @@ class ReactionController extends AbstractController
     /**
      * @Route("/reaction", name="reaction_addReaction", methods={"POST"})
      */
-    public function addReaction(Request $request, ReactionRepository $reactionRepository, SerializerInterface $serializer, EntityManagerInterface $em): Response
+    public function addReaction(Request $request, ReactionRepository $reactionRepository, UserRepository $userRepository, SerializerInterface $serializer, EntityManagerInterface $em): Response
     {
-        $data = $request->getContent();
-        /** @var Reaction $reaction */
-        $reaction = $serializer->deserialize($data, Reaction::class, 'json');
-        //$reaction->setSendTo($this->getUser());
+        $jsonData = $request->getContent();
+        $data = intval(json_decode($jsonData)->id);
+
+        // on créer un objet Reaction depuis le Json reçu :
+        ///** @var Reaction $reaction */
+        //$reaction = $serializer->deserialize($data, Reaction::class, 'json');
+        $reaction = new Reaction();
+        // on hydrate notre objet Reaction :
+        /** @var Profile $profileUserSendTo */
+        $profileUserSendTo = $this->getUser()->getProfile();
+        $reaction->setSendTo($profileUserSendTo);
+        /** @var User $userLiked */
+        $userLiked = $userRepository->find($data);
+        $reaction->setUserLiked($userLiked->getProfile());
         $reaction->setDateCreated(new \DateTime());
+        $reaction->setReciprocal(null);
 
         dd($reaction);
-        // TODO
-        return new Response([
-            'status' => 201,
-            'message' => 'Like send successfully !',
-            'data' => $reaction
-        ], 201);
+
+        //return new Response([
+        //    'status' => 201,
+        //    'message' => 'Like send successfully !',
+        //    'data' => $reaction
+        //], 201);
 
     }
 
